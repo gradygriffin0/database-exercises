a05e978d58acd1f13992061531fea784c99efea9
gradygriffin0 <grady.griffin0@gmail.com>
2021-07-26 12:14:51 -0500

    first

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    release_date DATE NOT NULL,
+    sales FLOAT,
+    genre VARCHAR(50),
+    PRIMARY KEY (id)
+                   );
