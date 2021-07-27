c95e15aacc571b527bff90bacf093dd567db2fdd
gradygriffin0 <grady.griffin0@gmail.com>
2021-07-27 11:03:07 -0500

    inserts 4 albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,9 +6,13 @@ CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
-    release_date DATE NOT NULL,
+    release_date INT NOT NULL,
     sales FLOAT,
     genre VARCHAR(50),
     PRIMARY KEY (id)
 
                    );
+
+DESCRIBE albums;
+
+SELECT * from albums;
